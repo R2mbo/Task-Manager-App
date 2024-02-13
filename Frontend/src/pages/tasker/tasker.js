f82082dab0b7017189492345fd1b91f8f415077f
Marwan Ashraf <xmarwanashraf@gmail.com>
2024-02-13 11:51:26 +0200

    Finished Everything Everything Works Correctly Now

--- Frontend/src/pages/tasker/tasker.js
+++ Frontend/src/pages/tasker/tasker.js
@@ -24,6 +24,11 @@ createTaskButton.addEventListener("click", function () {
 closeCard.addEventListener("click", function () {
     createTaskCard.classList.add("hidden")
 })
+let editCard = document.getElementById("edit-card");
+let closeEditCard = document.getElementById("close-edit-card");
+let editForm = document.getElementById("edit-task-form");
+let editSelect = document.querySelectorAll("select")[1];
+let taskEdit = document.getElementById("task-edit")
 
 // Check for Visibilty in Drop Down List and aside List and the create task card
 document.addEventListener("click", function (e) {
@@ -35,6 +40,9 @@ document.addEventListener("click", function (e) {
     }
     if (!e.target.closest("#create-card") && !e.target.matches("#create-task") && !createTaskCard.classList.contains("hidden")) {
         createTaskCard.classList.add("hidden")
+    }
+    if (!e.target.closest("#edit-card") && !e.target.matches(".fa-edit") && !editCard.classList.contains("hidden")) {
+        editCard.classList.add("hidden")
     }
 })
 
@@ -87,7 +95,6 @@ let veryBigDiv = document.createElement("div");
 veryBigDiv.className = "space-y-5"
 section.classList.add("mt-5")
 let h2 = document.createElement("h2");
-h2.className = "text-3xl mx-auto sm:w-[600px] font-bold mb-6";
 let token = localStorage.getItem("token");
 let id = localStorage.getItem("id")
 
@@ -100,7 +107,6 @@ try {
         }
     }).then(async (data) => {
         location.hash = "#all_tasks"
-        h2.innerHTML = "All Tasks";
         let userData = await data.json();
         let tasks = userData.data.docs;
         if (tasks.length === 0) {
@@ -113,6 +119,8 @@ try {
             h2.innerHTML = '';
             h2.className = '';
             veryBigDiv.innerHTML = '';
+            h2.innerHTML = "All Tasks";
+            h2.className = "text-3xl mx-auto sm:w-[600px] font-bold mb-6";
             for (let task of tasks) {
                 // the div after the very big div contains all the task items
                 let bigDiv = document.createElement("div");
@@ -135,16 +143,23 @@ try {
                 // label for the task
                 let taskText = document.createElement("label");
                 taskText.setAttribute("for", `${task.id}`);
-                taskText.className = "ml-2 py-3 w-full font-semibold text-xl"
+                taskText.className = "ml-2 py-3 w-full font-semibold text-[1rem] sm:text-xl"
                 taskText.innerHTML = `${task.description}`;
+                if (task.isDone) {
+                    checkBox.setAttribute("checked", "");
+                    taskText.classList.add("line-through");
+                } else {
+                    checkBox.removeAttribute("checked")
+                    taskText.classList.remove("line-through");
+                }
                 // Delete Button
                 let deleteButton = document.createElement("i")
                 deleteButton.className = "fas fa-times text-2xl cursor-pointer pt-[20px] ml-auto pt-4 text-gray-800 hover:text-black";
-                deleteButton.id = `${task.id}-delete`;
+                deleteButton.id = `${task.id}`;
                 // Edit Button
                 let editButton = document.createElement("i");
                 editButton.className = "fas fa-edit ml-5 pt-4 cursor-pointer text-gray-800 hover:text-black text-2xl";
-                editButton.id = `${task.id}-edit`
+                editButton.id = `${task.id}`
                 section.prepend(h2);
                 section.append(veryBigDiv);
                 veryBigDiv.append(bigDiv)
@@ -156,6 +171,60 @@ try {
                 bigDiv.append(deleteButton)
                 bigDiv.append(editButton)
                 main.append(section)
+                deleteButton.addEventListener("click", function () {
+                    fetch(`https://task-manager-api-fgcs.onrender.com/api/v1/tasks/${deleteButton.id}`, {
+                        method: "DELETE",
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                        },
+                    }).then(() => location.reload())
+                })
+
+                checkBox.addEventListener("click", function () {
+                    if (!checkBox.labels[0].classList.contains("line-through") && !checkBox.hasAttribute("checked")) {
+                        fetch(`https://task-manager-api-fgcs.onrender.com/api/v1/tasks/toggle-done-task/${checkBox.id}`, {
+                            method: "PATCH",
+                            headers: {
+                                Authorization: `Bearer ${token}`,
+                            }
+                        })
+                        checkBox.labels[0].classList.add("line-through")
+                        checkBox.setAttribute("checked", "")
+                    } else {
+                        fetch(`https://task-manager-api-fgcs.onrender.com/api/v1/tasks/toggle-done-task/${checkBox.id}`, {
+                            method: "PATCH",
+                            headers: {
+                                Authorization: `Bearer ${token}`,
+                            }
+                        })
+                        checkBox.labels[0].classList.remove("line-through")
+                        checkBox.removeAttribute("checked")
+                    }
+                })
+
+                editButton.addEventListener("click", function () {
+                    editCard.classList.remove("hidden")
+                    taskEdit.value = taskText.innerText;
+                    editSelect.value = spanInsideMediumDiv.innerHTML
+                    editForm.addEventListener("submit", function (e) {
+                        e.preventDefault();
+                        fetch(`https://task-manager-api-fgcs.onrender.com/api/v1/tasks/${checkBox.id}`, {
+                            method: "PATCH",
+                            body: JSON.stringify({
+                                description: taskEdit.value,
+                                category: editSelect.value
+                            }),
+                            headers: {
+                                Authorization: `Bearer ${token}`,
+                                "Content-type": "application/json",
+                            }
+                        }).then(() => location.reload())
+                        editCard.classList.add("hidden")
+                    })
+                    closeEditCard.addEventListener("click", function () {
+                        editCard.classList.add("hidden")
+                    })
+                })
             }
         }
     })
@@ -190,6 +259,8 @@ for (let li of list) {
                         h2.innerHTML = '';
                         h2.className = '';
                         veryBigDiv.innerHTML = '';
+                        h2.innerHTML = "All Tasks";
+                        h2.className = "text-3xl mx-auto sm:w-[600px] font-bold mb-6";
                         for (let task of tasks) {
                             // the div after the very big div contains all the task items
                             let bigDiv = document.createElement("div");
@@ -212,7 +283,7 @@ for (let li of list) {
                             // label for the task
                             let taskText = document.createElement("label");
                             taskText.setAttribute("for", `${task.id}`);
-                            taskText.className = "ml-2 py-3 w-full font-semibold text-xl"
+                            taskText.className = "ml-2 py-3 w-full font-semibold text-[1rem] sm:text-xl"
                             taskText.innerHTML = `${task.description}`;
                             // Delete Button
                             let deleteButton = document.createElement("i")
@@ -233,6 +304,35 @@ for (let li of list) {
                             bigDiv.append(deleteButton)
                             bigDiv.append(editButton)
                             main.append(section)
+                            deleteButton.addEventListener("click", function () {
+                                fetch(`https://task-manager-api-fgcs.onrender.com/api/v1/tasks/${deleteButton.id}`, {
+                                    method: "DELETE",
+                                    headers: {
+                                        Authorization: `Bearer ${token}`,
+                                    },
+                                }).then(() => location.reload())
+                            })
+                            checkBox.addEventListener("click", function () {
+                                if (!checkBox.labels[0].classList.contains("line-through") && !checkBox.hasAttribute("checked")) {
+                                    fetch(`https://task-manager-api-fgcs.onrender.com/api/v1/tasks/toggle-done-task/${checkBox.id}`, {
+                                        method: "PATCH",
+                                        headers: {
+                                            Authorization: `Bearer ${token}`,
+                                        }
+                                    })
+                                    checkBox.labels[0].classList.add("line-through")
+                                    checkBox.setAttribute("checked", "")
+                                } else {
+                                    fetch(`https://task-manager-api-fgcs.onrender.com/api/v1/tasks/toggle-done-task/${checkBox.id}`, {
+                                        method: "PATCH",
+                                        headers: {
+                                            Authorization: `Bearer ${token}`,
+                                        }
+                                    })
+                                    checkBox.labels[0].classList.remove("line-through")
+                                    checkBox.removeAttribute("checked")
+                                }
+                            })
                         }
                     }
                 })
@@ -249,8 +349,7 @@ for (let li of list) {
                     }
                 }).then(async (data) => {
 
-                    location.hash = `#${li.innerHTML}`
-                    h2.innerHTML = `${li.innerHTML}`;
+                    location.hash = `#${li.innerHTML.toLowerCase()}`
                     let userData = await data.json();
                     let tasks = userData.data.docs;
 
@@ -265,6 +364,8 @@ for (let li of list) {
                         h2.innerHTML = '';
                         h2.className = '';
                         veryBigDiv.innerHTML = '';
+                        h2.innerHTML = `${li.innerHTML}`;
+                        h2.className = "text-3xl mx-auto sm:w-[600px] font-bold mb-6";
                         for (let task of tasks) {
                             // the div after the very big div contains all the task items
                             let bigDiv = document.createElement("div");
@@ -287,7 +388,7 @@ for (let li of list) {
                             // label for the task
                             let taskText = document.createElement("label");
                             taskText.setAttribute("for", `${task.id}`);
-                            taskText.className = "ml-2 py-3 w-full font-semibold text-xl"
+                            taskText.className = "ml-2 py-3 w-full font-semibold text-[1rem] sm:text-xl"
                             taskText.innerHTML = `${task.description}`;
                             // Delete Button
                             let deleteButton = document.createElement("i")
@@ -308,6 +409,35 @@ for (let li of list) {
                             bigDiv.append(deleteButton)
                             bigDiv.append(editButton)
                             main.append(section)
+                            deleteButton.addEventListener("click", function () {
+                                fetch(`https://task-manager-api-fgcs.onrender.com/api/v1/tasks/${deleteButton.id}`, {
+                                    method: "DELETE",
+                                    headers: {
+                                        Authorization: `Bearer ${token}`,
+                                    },
+                                }).then(() => location.reload())
+                            })
+                            checkBox.addEventListener("click", function () {
+                                if (!checkBox.labels[0].classList.contains("line-through") && !checkBox.hasAttribute("checked")) {
+                                    fetch(`https://task-manager-api-fgcs.onrender.com/api/v1/tasks/toggle-done-task/${checkBox.id}`, {
+                                        method: "PATCH",
+                                        headers: {
+                                            Authorization: `Bearer ${token}`,
+                                        }
+                                    })
+                                    checkBox.labels[0].classList.add("line-through")
+                                    checkBox.setAttribute("checked", "")
+                                } else {
+                                    fetch(`https://task-manager-api-fgcs.onrender.com/api/v1/tasks/toggle-done-task/${checkBox.id}`, {
+                                        method: "PATCH",
+                                        headers: {
+                                            Authorization: `Bearer ${token}`,
+                                        }
+                                    })
+                                    checkBox.labels[0].classList.remove("line-through")
+                                    checkBox.removeAttribute("checked")
+                                }
+                            })
                         }
                     }
                 })
@@ -320,3 +450,29 @@ for (let li of list) {
 }
 
 
+// Create New Task Form 
+
+let createTaskForm = document.getElementById("create-task-form");
+let selectList = document.querySelectorAll("select")[0];
+let taskDescription = document.getElementById("task");
+createTaskForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    try {
+        fetch("https://task-manager-api-fgcs.onrender.com/api/v1/tasks/", {
+            method: "POST",
+            body: JSON.stringify({
+                description: taskDescription.value,
+                category: selectList.value,
+            }),
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-type": "application/json",
+            }
+        }).then(() => {
+            createTaskCard.classList.add("hidden")
+            location.reload()
+        })
+    } catch (err) {
+        console.log(err)
+    }
+})
